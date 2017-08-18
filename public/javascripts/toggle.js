ac897d5aa254ea958ac14366ad9d061d3cae3dc2
kayanhales <daybreakbliss@gmail.com>
2017-08-18 10:39:36 -0400

    init commit

---
+++ public/javascripts/toggle.js
@@ -0,0 +1,7 @@
+function toggleIt() {
+       var e = document.getElementById('p');
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
